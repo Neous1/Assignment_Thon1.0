84015dd3dc64c9988300a76196ccba4e9217266b
Neous1 <Neous1@users.noreply.github.com>
2017-07-20 16:04:52 -0400

    testing innerHtml

--- js/app.js
+++ js/app.js
@@ -19,13 +19,64 @@ var rental = {
         price:21.99
     }
 
-],
+    ],
+    displayAll: function(){
+        var len = rental.vehicles.length;
+        for (var i =0; i<len;i++){
+            console.log(rental.vehicles[i])
+        }
 
-displayType: function(){
-    console.log(rental.vehicles)
-    // console.log("hi");
-}
+    },
+    displayType: function(){
+        var len = rental.vehicles.length;
+        for (var i =0; i<len;i++){
+            console.log(rental.vehicles[i].type)
+        }
+
+    },
+    displayPrice: function(){
+        var len = rental.vehicles.length;
+        for (var i =0; i<len;i++){
+            console.log(rental.vehicles[i].price)
+        }
+
+    },
+
+    displayQty: function(){
+        var len = rental.vehicles.length;
+        for (var i =0; i<len;i++){
+            console.log(rental.vehicles[i].qty)
+        }
+
+    },
+    availability : function(){
+        var len = rental.vehicles.length;
+        for (var i =0; i<len;i++){
+            
+            if (rental.vehicles[i].qty <= 0){
+                rental.vehicles[i].avail = false;
+            }
+            console.log(rental.vehicles[i].avail)
+        }
+
+    }
 
 };
 
+function rentSelect (){
+    document.getElementById("sedan").innerHTML = "";
+}
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+}
+
+document.getElementById('demo').value = rental.displayType;
+
+// console.log(rental.vehicles[0].type)
+rental.displayAll();
 rental.displayType();
+rental.displayPrice();
+rental.displayQty();
+rental.availability();
